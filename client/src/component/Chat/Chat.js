bb1a9a2a0551b7ac96a43cf6712bb8cefe784cee
Eddierwasif <eddierwasifh1@gmail.com>
2023-07-06 15:05:51 +0800

    Update client and server modules

--- client/src/component/Chat/Chat.js
+++ client/src/component/Chat/Chat.js
@@ -1,32 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import queryString from 'query-string';
-import io from 'socket.io-client';
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import queryString from "query-string";
+import io from "socket.io-client";
 
-import './Chat.css';
-import InfoBar from '../info/InfoBar';
-import Input from '../Input/Input';
-import Messages from '../Messages/Messages';
-import TextContainer from '../TextContainer/TextContainer';
+import "./Chat.css";
+import InfoBar from "../info/InfoBar";
+import Input from "../Input/Input";
+import Messages from "../Messages/Messages";
+import TextContainer from "../TextContainer/TextContainer";
 
 let socket;
 
 const Chat = () => {
   const location = useLocation();
-  const [name, setName] = useState('');
-  const [room, setRoom] = useState('');
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
   const [users, setUsers] = useState([]);
   const [messages, setMessages] = useState([]);
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
 
-    socket = io();
-
+    socket = io("http://localhost:5000");
     setName(name);
     setRoom(room);
-    socket.emit('join', { name, room }, () => {});
+    socket.emit("join", { name, room });
 
     return () => {
       socket.disconnect();
@@ -35,13 +34,13 @@ const Chat = () => {
   }, [location.search]);
 
   useEffect(() => {
-    socket.on('message', (message) => {
+    socket.on("message", (message) => {
       setMessages((messages) => [...messages, message]);
     });
-    socket.on('roomData', ({ users }) => {
+    socket.on("roomData", ({ users }) => {
       setUsers(users);
     });
-    socket.on('locationMessage', ({ user, url }) => {
+    socket.on("locationMessage", ({ user, url }) => {
       const message = {
         user,
         text: url,
@@ -49,7 +48,7 @@ const Chat = () => {
       };
       setMessages((messages) => [...messages, message]);
     });
-    socket.on('imageMessage', ({ user, image }) => {
+    socket.on("imageMessage", ({ user, image }) => {
       const message = {
         user,
         image,
@@ -57,28 +56,30 @@ const Chat = () => {
       };
       setMessages((messages) => [...messages, message]);
     });
+    socket.on("unsendMessage", (messageId) => {
+      setMessages((messages) => messages.filter((msg) => msg.id !== messageId));
+    });
 
     return () => {
-      socket.off('message');
-      socket.off('roomData');
-      socket.off('locationMessage');
-      socket.off('imageMessage');
+      socket.off("message");
+      socket.off("roomData");
+      socket.off("locationMessage");
+      socket.off("imageMessage");
+      socket.off("unsendMessage");
     };
   }, []);
 
   const sendMessage = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (message) {
-        socket.emit('sendMessage', message, () => {
-            setMessage('')
-        })
+      socket.emit("sendMessage", message);
+      setMessage("");
     }
-}
-console.log(message, messages);
+  };
 
   const sendLocation = () => {
     if (!navigator.geolocation) {
-      return alert('Geolocation is not supported by your browser');
+      return alert("Geolocation is not supported by your browser");
     }
 
     navigator.geolocation.getCurrentPosition(
@@ -87,35 +88,53 @@ console.log(message, messages);
           latitude: position.coords.latitude,
           longitude: position.coords.longitude,
         };
-        socket.emit('sendLocation', location);
+        socket.emit("sendLocation", location);
       },
       () => {
-        alert('Unable to retrieve your location');
+        alert("Unable to retrieve your location");
       }
     );
   };
 
   const sendImage = (image) => {
-    socket.emit('sendImage', image);
+    socket.emit("sendImage", image);
   };
 
-  return (
-    <div className="outerContainer">
-      <div className="container">
-        <InfoBar room={room} />
-        <Messages messages={messages} name={name} />
-        <Input
-          message={message}
-          setMessage={setMessage}
-          sendMessage={sendMessage} 
-          sendLocation={sendLocation}
-          sendImage={sendImage}
-        />
-      </div>
-      <TextContainer users={users} room={room} />
+  const unsendMessage = (index) => {
+    const message = messages[index];
+    if (message && message.id) {
+      socket.emit("unsendMessage", message.id);
+    }
+  };
+
+  const unsendImage = (index) => {
+	const message = messages[index];
+	if (message && message.id) {
+	  socket.emit("unsendImage", message.id);
+	}
+  };
+
+	return (
+		<div className="outerContainer">
+    <div className="container">
+      <InfoBar room={room} />
+      <Messages
+        messages={messages}
+        name={name}
+        unsendMessage={unsendMessage}
+        unsendImage={unsendImage}  
+      />
+      <Input
+        message={message}
+        setMessage={setMessage}
+        sendMessage={sendMessage}
+        sendLocation={sendLocation}
+        sendImage={sendImage}
+      />
     </div>
-  );
-  
-};
+		<TextContainer users={users} room={room} />
+		</div>
+	);
+	};
 
 export default Chat;
--- server/index.js
+++ server/index.js
@@ -1,62 +1,107 @@
-const express = require('express');
-const socketio = require('socket.io');
-const http = require('http');
-const path = require('path');
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
-const { addUser, getUser, getUsersInRoom, removeUser } = require('./user');
-const router = require('./router');
+const { addUser, getUser, getUsersInRoom, removeUser } = require("./user");
+const router = require("./router");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = require("socket.io")(server);
 
-app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(express.static(path.join(__dirname, "..", "build")));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
-io.on('connection', (socket) => {
-  socket.on('join', ({ name, room }, callback) => {
-    const { error, user } = addUser({ id: socket.id, name, room });
-    if (error) return callback(error);
+io.on("connection", (socket) => {
+  console.log("Connected to the server");
 
-    socket.emit('message', { user: 'admin', text: `${user.name}! Welcome to ${user.room}` });
-    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined` });
+  socket.on("join", ({ name, room }) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) {
+      return socket.emit("error", { message: error }); // Emit an error event if there's an error during user addition
+    }
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to ${user.room}`,
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} has joined` });
     socket.join(user.room);
 
-    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
-
-    callback();
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
   });
 
-  socket.on('sendMessage', (message, callback) => {
+  socket.on("sendMessage", (message) => {
     const user = getUser(socket.id);
-    io.to(user.room).emit('message', { user: user.name, text: message });
-    callback();
+    if (user) {
+      const messageId = uuidv4();
+      io.to(user.room).emit("message", {
+        id: messageId,
+        user: user.name,
+        text: message,
+      });
+    }
   });
 
-  socket.on('sendLocation', ({ latitude, longitude }, callback) => {
+  socket.on("sendLocation", ({ latitude, longitude }, callback) => {
     const user = getUser(socket.id);
-    io.to(user.room).emit('locationMessage', { user: user.name, url: `https://www.google.com/maps?q=${latitude},${longitude}` });
-    callback();
+    if (user) {
+      io.to(user.room).emit("locationMessage", {
+        user: user.name,
+        url: `https://www.google.com/maps?q=${latitude},${longitude}`,
+      });
+      callback();
+    }
   });
 
-  socket.on('sendImage', (image, callback) => {
+  socket.on("sendImage", (image, callback) => {
     const user = getUser(socket.id);
-    io.to(user.room).emit('imageMessage', { user: user.name, image });
-    callback();
+    if (user) {
+      io.to(user.room).emit("imageMessage", { user: user.name, image });
+      if (typeof callback === "function") {
+        callback();
+      }
+    }
   });
 
-  socket.on('disconnect', () => {
+  socket.on("unsendMessage", (messageId) => {
+    const user = getUser(socket.id);
+    if (user) {
+      io.to(user.room).emit("unsendMessage", messageId);
+    }
+  });
+
+  socket.on("unsendImage", (messageId) => {
+	const user = getUser(socket.id);
+	io.to(user.room).emit("unsendImage", messageId);
+  });
+
+  socket.on("disconnect", () => {
     const user = removeUser(socket.id);
     if (user) {
-      io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left!` });
-      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+      io.to(user.room).emit("message", {
+        user: "admin",
+        text: `${user.name} has left!`,
+      });
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
     }
   });
 });
+
 app.use(router);
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
